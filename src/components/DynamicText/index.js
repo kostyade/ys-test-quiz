ff0b0f0febea418be79fd67fb9d426179514358b
Konstantin Degtyarenko <kdegtyarenko@dataart.com>
2019-06-20 22:15:41 +0300

    formatting

--- src/components/DynamicText/index.js
+++ src/components/DynamicText/index.js
@@ -8,9 +8,7 @@ class DynamicText extends React.Component {
 
   render() {
     return (
-      <span className={this.props.className}
-        ref={el => this.el = el}
-      >
+      <span className={this.props.className} ref={el => (this.el = el)}>
         {this.props.children}
       </span>
     );
